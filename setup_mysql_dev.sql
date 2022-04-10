f7e7e24b90bd2179a249e7d44c59eb29c648ec81
sl4 <s4lv4dord@gmail.com>
2022-04-10 11:17:18 -0700

    fixing privileges and comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,9 @@
+-- prepares a MySQL server for the project
+-- create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- create user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL ON hbnb_dev.* TO 'hbnb_dev'@'localhost';
+-- user privileges on hbnb_dev_db
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- user privileges on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
