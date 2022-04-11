c5f1aa30fdb592d13b0c632310a486bd255657eb
Santi3948 <santiagonegreyra@gmail.com>
2022-04-11 08:31:45 -0700

    update city and state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    name = Column(String(128), nullable=False)
+    __tablename__ = "states"
+    cities = relationship("City", backref="states", cascade="all, delete-orphan")
+    @property
+    def cities(self):
+        return self.cities.format(state.id = state_id)
