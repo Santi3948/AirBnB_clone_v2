863774d8ecc062eb46968e50d331482db97ae66b
Tukson <santiagonegreyra@gmail.com>
2022-05-14 19:51:35 +0000

    3-python_route.py working

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""hbnb module"""
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello2():
+    """Hello HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def hello3(text):
+    """Hello var"""
+    return 'C %s' % text.replace("_", " ")
+
+
+@app.route('/number/<int:n>')
+def hello4(n):
+    """Hello n"""
+    return '%d is a number' % n
+
+
+if __name__ == "__main__":
+    app.run()
