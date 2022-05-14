88a4415e3deacc52019f6e46991abfe7b8ba4d9e
Tukson <santiagonegreyra@gmail.com>
2022-05-14 19:04:00 +0000

    fixing pep8 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ from flask import Flask, escape, request
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello():
     """Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     app.run()
